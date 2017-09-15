1a9327b39882c9621bddcb9cae77d026ef0b6357
João Belo <jpasbelo@gmail.com>
2017-09-15 02:16:59 +0100

    index e config

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+ini_set( "display_errors", true );
+date_default_timezone_set( "Europe/Lisbon" );
+
+$host 	= 	$GLOBALS["host"]	= 'localhost';  // or remote address
+$user 	= 	$GLOBALS["user"]	= 'username';
+$pass 	= 	$GLOBALS["pass"]	= 'password';
+$dbname = 	$GLOBALS["dbname"]	= 'database';
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	include_once('config.php');
+	include_once('classes/connect.php');
+	include_once('classes/insertdb.php');
+	include_once('classes/removedb.php');
+	include_once('classes/retrievefromdb.php');
+	include_once('classes/updatedb.php');
+?>
+
+MAKE STUFF
